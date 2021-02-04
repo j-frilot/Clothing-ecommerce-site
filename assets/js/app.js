f83fa10c952f2b062fc0a1beae25951b76f8b15a
j-frilot <frilotdr@gmail.com>
2021-02-04 14:50:06 -0600

    recovered

---
+++ assets/js/app.js
@@ -0,0 +1,173 @@
+//SLIDE SHOW
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    let firstSlide = document.querySelector(".first-slide")
+    firstSlide.style.display = "flex"
+})
+
+let slides = [
+    "url('images/home/slide-1.jpg')", 
+    "url('images/home/slide-2.jpg')", 
+    "url('images/home/slide-3.jpg')", 
+];
+
+let picOne = slides[0];
+let picTwo = slides[1];
+let picThree = slides[2];
+
+let slideShow = document.querySelector(".slide-show");
+
+slideCount = 0
+function autoSlide () {
+
+    // left.style.display = "none";
+    // right.style.display = "none";
+
+    slideImage = slideShow.style.backgroundImage = slides[slideCount]
+    slideCount ++;
+
+    if ( slideCount >= slides.length) {
+        slideCount = 0;
+    }
+}
+setInterval(autoSlide, 3000);
+
+// END SLIDE SHOW CONTENT
+
+
+
+
+
+// PRODUCTS
+
+let products = [{
+    id: 7,
+    name : "Jack",
+    age : "4 years",
+    breed : "Shar-pei",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "Black",
+    img: "images/inventory/shar-pei.jpeg",
+    }, 
+    {
+    id: 8,
+    name : "Ellie",
+    age : "1 years",
+    breed : "Terrier Mix",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color: "black",
+    img: "images/inventory/terrier-mix.jpeg",
+    }, 
+    {
+    id: 9,
+    name : "Thor",
+    age : "6 years",
+    breed : "Sheppard",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "brown",
+    img: "images/inventory/shepard.jpeg",
+}, 
+    {
+    id: 10,
+    name : "Chloe",
+    age : "3 years",
+    breed : "Husky",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color: "Tan, White",
+    img: "images/inventory/dog-10.jpg",
+    }, 
+    {
+    id: 11,
+    name : "Toby",
+    age : "5 years",
+    breed : "Pit Bull Terrier",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "brown",
+    img: "images/inventory/dog-11.jpg", 
+    },  
+    {
+    id: 12,
+    name : "Holly",
+    age : "4 years",
+    breed : "Shepard",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    img: "images/inventory/dog-12.jpg",
+    color: "tan"
+    }]
+
+window.addEventListener('DOMContentLoaded', function(){
+    let inventory = document.querySelector(".inventory");
+
+    let showProducts = products.map(function(product){
+        return `<div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src=${product.img} alt="product-1" class="img product-pic">
+                        </div>
+                        
+
+                        <div class="card-head">
+                            <h5><a href="#">${product.name}</a> </h5>
+                            <h5>${product.gender}</h5>
+                        </div>
+
+                        <div class="card-body">
+                                <div class="desc"><p>${product.desc}</p></div>
+                            </div>
+
+                        <div class="card-footer">
+                            <h5>${product.age} old</h5>
+                            <div class="breed"><h5>${product.breed}</h5></div>
+                        </div>
+                    </div>
+                </div>`
+    });
+    showProducts = showProducts.join("");
+    inventory.innerHTML = showProducts;
+
+});
+
+
+/////number incrementer
+
+let rescueNum = document.querySelector(".rescue-num");
+let adoptNum = document.querySelector(".adopt-num");
+let volunteerNum = document.querySelector(".volunteer-num");
+
+
+
+
+let rescueCount = 1;
+setInterval(()=>{
+    if (rescueCount < 342){
+        rescueCount++;
+        rescueNum.innerText = rescueCount;
+    }
+}, 3)
+
+let adoptCount = 1;
+setInterval(()=>{
+    if (adoptCount < 155){
+        adoptCount++;
+        adoptNum.innerText = adoptCount;
+    }
+}, 10)
+
+let volunteerCount = 1;
+setInterval(()=>{
+    if (volunteerCount < 50){
+        volunteerCount++;
+        volunteerNum.innerText = volunteerCount;
+    }
+}, 50)
+
+// setTimeout(()=>{
+//     followers.innerText = "Followers on Instagram!"
+// },4700)
---
+++ assets/js/inventory.js
@@ -0,0 +1,154 @@
+let products = [{
+    id: 1,
+    name : "Max",
+    age : "2 years",
+    breed : "Pit Bull",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "Brown",
+    img: "images/inventory/pit.jpg",
+    }, 
+    {
+    id: 2,
+    name : "Bella",
+    age : "Unknown",
+    breed : "Chihuahua",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color:"tan",
+    img: "images/inventory/chihuahua.jpeg",
+    }, 
+    {
+    id: 3,
+    name : "Buddy",
+    age : "4 years",
+    breed : "Hound",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "Black, White",
+    img: "images/inventory/hound.jpeg",
+    }, 
+    {
+    id: 4,
+    name : "Daisy",
+    age : "2 years",
+    breed : "Labrador",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color: "Brown",
+    img: "images/inventory/labrador.jpeg",
+    }, 
+    {
+    id: 5,
+    name : "Charlie",
+    age : "1 years",
+    breed : "Pitbull, Boxer",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "brown",
+    img: "images/inventory/pit-boxer.jpg", 
+    },  
+    {
+    id: 6,
+    name : "Coco",
+    age : "5 years",
+    breed : "Poodle",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color: "white",
+    img: "images/inventory/poodle.jpg",
+    },
+    {
+    id: 7,
+    name : "Jack",
+    age : "4 years",
+    breed : "Shar-pei",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "Black",
+    img: "images/inventory/shar-pei.jpeg",
+    }, 
+    {
+    id: 8,
+    name : "Ellie",
+    age : "1 years",
+    breed : "Terrier Mix",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color: "black",
+    img: "images/inventory/terrier-mix.jpeg",
+    }, 
+    {
+    id: 9,
+    name : "Thor",
+    age : "6 years",
+    breed : "Sheppard",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "brown",
+    img: "images/inventory/shepard.jpeg",
+}, 
+    {
+    id: 10,
+    name : "Chloe",
+    age : "3 years",
+    breed : "Husky",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "female",
+    color: "Tan, White",
+    img: "images/inventory/dog-10.jpg",
+    }, 
+    {
+    id: 11,
+    name : "Toby",
+    age : "5 years",
+    breed : "Pit Bull Terrier",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    color: "brown",
+    img: "images/inventory/dog-11.jpg", 
+    },  
+    {
+    id: 12,
+    name : "Holly",
+    age : "4 years",
+    breed : "Shepard",
+    desc : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+    gender : "male",
+    img: "images/inventory/dog-12.jpg",
+    color: "tan"
+    }]
+
+window.addEventListener('DOMContentLoaded', () => {
+    let inventory = document.querySelector(".inventory");
+
+    let showProducts = products.map((product) => {
+        return `<div class="card">
+                    <div class="card-img">
+                        <img src=${product.img} alt="product-1" class="img product-pic">
+                    </div>
+    
+
+                    <div class="card-head">
+                        <h5><a href="#">${product.name}</a> </h5>
+                        <h5>${product.gender}</h5>
+                    </div>
+
+                    <div class="card-body">
+                        <div class="desc"><p>${product.desc}</p></div>
+                    </div>
+
+                    <div class="card-footer">
+                        <h5>${product.age}</h5>
+                        <div class="breed"><h5>${product.breed}</h5></div>
+                    </div>
+        
+                </div>`
+    });
+    showProducts = showProducts.join("");
+    inventory.innerHTML = showProducts;
+
+});
+
+
+
