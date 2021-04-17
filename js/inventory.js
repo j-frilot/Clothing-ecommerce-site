f6674ed3447d417730b5c1ccc95773d448b3141b
j-frilot <frilotdr@gmail.com>
2021-04-16 21:15:49 -0500

    add filter

--- js/inventory.js
+++ js/inventory.js
@@ -1,4 +1,79 @@
-let products = [
+// PRODUCTS
+//home products
+let homeProducts = [
+    {
+        id: 7,
+        name: "Jack",
+        age: "4 years",
+        breed: "Shar-pei",
+        desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+        gender: "male",
+        color: "Black",
+        img: "images/inventory/dog_7.jpeg"
+    },
+    {
+        id: 8,
+        name: "Ellie",
+        age: "1 years",
+        breed: "Terrier Mix",
+        desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+        gender: "female",
+        color: "black",
+        img: "images/inventory/dog_8.jpeg"
+    },
+    {
+        id: 9,
+        name: "Thor",
+        age: "6 years",
+        breed: "Sheppard",
+        desc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, in?",
+        gender: "male",
+        color: "brown",
+        img: "images/inventory/dog_9.jpeg"
+    }
+];
+
+// home inventory map
+window.addEventListener("DOMContentLoaded", () => {
+    let homeInventory = document.querySelector(".homeInventory");
+
+    let showHomeProducts = homeProducts.map((homeProduct) => {
+        return `<div class="card">
+                    <div class="card-img">
+                        <img src=${homeProduct.img} alt="product-1" class="img product-pic">
+                    </div>
+
+                    <div class="card-name">
+                            <h1>${homeProduct.name}</h1>
+                        </div>
+                    <div class="info">
+                        
+
+                        <div class="card-age">
+                            <h3>${homeProduct.age}</h3>
+                        </div>
+
+                        <div class="card-gender">
+                            <h3>${homeProduct.gender}</h3>
+                        </div>
+
+                        <div class="card-breed">
+                            <h3>${homeProduct.breed}</h3>
+                        </div>
+
+                        <a href="#" class="card-dog-link"><h3>More Info</h3></a>
+                    </div>
+                </div>`;
+    });
+    showHomeProducts = showHomeProducts.join("");
+    homeInventory.innerHTML = showHomeProducts;
+});
+
+//all products
+let allProducts = [
     {
         id: 1,
         name: "Max",
@@ -97,7 +172,7 @@ let products = [
         gender: "male",
         color: "brown",
         img: "images/inventory/dog_9.jpeg"
-    }, //REPEAT////////////////////////////////
+    },
     {
         id: 1,
         name: "Max",
@@ -199,41 +274,85 @@ let products = [
     }
 ];
 
+// full inventory map
 window.addEventListener("DOMContentLoaded", () => {
-    let inventory = document.querySelector(".inventory");
+    const fullInventory = document.querySelector(".full-inventory");
 
-    let showProducts = products.map((product) => {
+    let showAllProducts = allProducts.map((eachProduct) => {
         return `<div class="card">
                     <div class="card-img">
-                        <img src=${product.img} alt="product-1" class="img product-pic">
+                        <img src=${eachProduct.img} alt="product-1" class="img product-pic">
                     </div>
 
                     <div class="card-name">
-                            <h1>${product.name}</h1>
+                            <h1>${eachProduct.name}</h1>
                         </div>
                     <div class="info">
-                        
 
                         <div class="card-age">
-                            <h3>${product.age}</h3>
+                            <h3>${eachProduct.age}</h3>
                         </div>
 
                         <div class="card-gender">
-                            <h3>${product.gender}</h3>
+                            <h3>${eachProduct.gender}</h3>
                         </div>
 
                         <div class="card-breed">
-                            <h3>${product.breed}</h3>
+                            <h3>${eachProduct.breed}</h3>
                         </div>
 
                         <a href="#" class="card-dog-link"><h3>More Info</h3></a>
                     </div>
-                    
-
-                    
-
                 </div>`;
     });
-    showProducts = showProducts.join("");
-    inventory.innerHTML = showProducts;
+    showAllProducts = showAllProducts.join("");
+    fullInventory.innerHTML = showAllProducts;
+});
+
+let blacks = [];
+let browns = [];
+let whites = [];
+let otherColors = [];
+
+// SORTING COLORS
+allProducts.forEach((dog) => {
+    dog.color.toLowerCase() == "black".toLowerCase()
+        ? blacks.push(dog.name)
+        : dog.color.toLowerCase() == "brown".toLowerCase()
+        ? browns.push(dog.name)
+        : dog.color.toLowerCase() == "white".toLowerCase()
+        ? whites.push(dog.name)
+        : otherColors.push(dog.name);
+});
+
+// SEARCH FORM
+
+const submit = document.querySelector(".form-submit");
+
+// SORTING MALES / FEMALES
+
+submit.addEventListener("click", () => {
+    const genderBtns = document.querySelectorAll('input[name="gender"]');
+    let selectedGender;
+    for (let genderBtn of genderBtns) {
+        if (genderBtn.checked) {
+            selectedGender = genderBtn.value;
+            break;
+        }
+    }
+
+    for (i = 0; i <= allProducts.length; i++) {
+        if (!allProducts.gender.toLowerCase() == selectedGender.toLowerCase()) {
+            allProducts[i].style.display = hide;
+        }
+    }
+
+    // let males = [];
+    // let females = [];
+
+    // selectedGender.toLowerCase() == "male".toLowerCase()
+    //     ? males.push(dog.name)
+    //     : females.push(dog.name);
+
+    // console.log(males);
 });
